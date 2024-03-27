42d72d0898f6fcb5c44260994b62d05b49379a9d
Nicos MUNTU <nicos19@hotmail.fr>
2024-03-27 15:16:19 +0100

    ternaire

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,46 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
-    return (
-      <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-        alt="bulbasur"></img>
-        <figcaption>Bulbasur</figcaption>
-      </figure>
-    );
-  }
-  
-  export default PokemonCard;
+  const pokemon = pokemonList[0];
+  return (
+    
+    (!pokemon.imgSrc)
+    ? <> <p> ??? </p> <figcaption>{pokemon.name}</figcaption> </>
+    : <> <img src={pokemon.imgSrc} /> <figcaption>{pokemon.name}</figcaption> </>
+
+
+)}
+  // pokemon ()
+  // console.log(pokemon.name);
+  // console.log(pokemon.imgSrc);
+
+  // Solution condition if
+  // if (!pokemon.imgSrc) {
+  //   return (
+  //     <>
+  //       <p>???</p>
+  //       <figcaption>{pokemon.name}</figcaption>
+  //     </>
+  //   );
+  // } else {
+  //   return (
+  //     <figure>
+  //       <img src={pokemon.imgSrc} />
+  //       <figcaption>{pokemon.name}</figcaption>
+  //     </figure>
+  //   )};
+
+  // A faire pour la quête condition ternaire
+
+
+
+export default PokemonCard;
