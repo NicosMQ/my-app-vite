0d799bbb53dd965baeb1f75e4a8fb4dc08fe811f
Nicos MUNTU <nicos19@hotmail.fr>
2024-03-28 16:02:00 +0100

    quete 5

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@ import PokemonCard from "./components/PokemonCard";
 
 
 
+
 function App() {
   const pokemonList = [
     {
-      name: "bulbasaur",
+      name: "Bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
-      name: "mew",
+      name: "Mew",
+      imgSrc:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png"
     },
   ];
   return (
     <>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[1]} />
     </>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,37 +1,53 @@
+import PropTypes from "prop-types";
 
-function PokemonCard({pokemon}){
+function PokemonCard({ pokemon }) {
+  
 
-  return (
-    
-    (!pokemon.imgSrc)
-    ? <> <p> ??? </p> <figcaption>{pokemon.name}</figcaption> </>
-    : <> <img src={pokemon.imgSrc} /> <figcaption>{pokemon.name}</figcaption> </>
+  return !pokemon.imgSrc ? (
+    <>
+      {" "}
+      <p> ??? </p>{" "}
+      <figcaption>
+        <h1>{pokemon.name}</h1>
+      </figcaption>{" "}
+    </>
+  ) : (
+    <>
+      {" "}
+      <img src={pokemon.imgSrc} />{" "}
+      <figcaption>
+        <h1>{pokemon.name}</h1>
+      </figcaption>{" "}
+    </>
+  );
+}
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
+// pokemon ()
+// console.log(pokemon.name);
+// console.log(pokemon.imgSrc);
 
-)}
+// Solution condition if
+// if (!pokemon.imgSrc) {
+//   return (
+//     <>
+//       <p>???</p>
+//       <figcaption>{pokemon.name}</figcaption>
+//     </>
+//   );
+// } else {
+//   return (
+//     <figure>
+//       <img src={pokemon.imgSrc} />
+//       <figcaption>{pokemon.name}</figcaption>
+//     </figure>
+//   )};
 
-  // pokemon ()
-  // console.log(pokemon.name);
-  // console.log(pokemon.imgSrc);
-
-  // Solution condition if
-  // if (!pokemon.imgSrc) {
-  //   return (
-  //     <>
-  //       <p>???</p>
-  //       <figcaption>{pokemon.name}</figcaption>
-  //     </>
-  //   );
-  // } else {
-  //   return (
-  //     <figure>
-  //       <img src={pokemon.imgSrc} />
-  //       <figcaption>{pokemon.name}</figcaption>
-  //     </figure>
-  //   )};
-
-  // A faire pour la quête condition ternaire
-
-
+// A faire pour la quête condition ternaire
 
 export default PokemonCard;
