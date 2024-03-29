657253889fef6c6003ee0cb1021bee08cbd85c2b
Nicos MUNTU <nicos19@hotmail.fr>
2024-03-29 15:48:02 +0100

    quete state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import PokemonCard from "./components/PokemonCard";
-
-
-
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [pokemonIndex, setpokemonIndex] = useState(0);
   const pokemonList = [
     {
       name: "Bulbasaur",
@@ -12,12 +12,39 @@ function App() {
     },
     {
       name: "Mew",
-      imgSrc:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png"
+      imgSrc: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
     },
   ];
   return (
     <>
-      <PokemonCard pokemon={pokemonList[1]} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+
+      {pokemonIndex > 0 ? (
+        <button onClick={() => setpokemonIndex(pokemonIndex - 1)}>
+          Précedent
+        </button>
+      ) : null}
+
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={() => setpokemonIndex(pokemonIndex + 1)}>
+          Suivant
+        </button>
+      )}
     </>
   );
 }
